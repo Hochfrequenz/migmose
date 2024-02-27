d2dd3145a395aa47bd0167c77ce6739f5ac67c56
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2024-02-27 14:07:07 +0100

    fix minor issue

--- src/migmose/mig/segmentgruppe.py
+++ src/migmose/mig/segmentgruppe.py
@@ -1,14 +1,13 @@
 """
 contains class for structured segmentgroups in mig tables. builds table recursively.
 """
+from migmose.mig.nachrichtenstrukturzeile import NachrichtenstrukturZeile
 
-from migmose.mig.zeile import NachrichtenStrukturZeile
 
-
-class SegmentGruppe(NachrichtenStrukturZeile):
+class SegmentGruppe(NachrichtenstrukturZeile):
     """
     class for structured segmentgroups in mig tables. builds table recursively. inherits from NachrichtenstrukturZeile
     """
 
-    segmente: list[NachrichtenStrukturZeile]
+    segmente: list[NachrichtenstrukturZeile]
     segmentgruppe: list["SegmentGruppe"]
