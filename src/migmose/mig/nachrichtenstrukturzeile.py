76cd2708bac24987e88a62866b7581c11df88239
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-03-04 11:14:00 +0100

    added examples to some docstrings

--- src/migmose/mig/nachrichtenstrukturzeile.py
+++ src/migmose/mig/nachrichtenstrukturzeile.py
@@ -7,7 +7,18 @@ from pydantic import BaseModel
 
 class NachrichtenstrukturZeile(BaseModel):
     """
-    class for lines in mig tables
+    class for lines in mig tables, e.g. (ORDCHG):
+    {
+        "zaehler": "0010",
+        "nr": "1",
+        "bezeichnung": "UNH",
+        "standard_status": "M",
+        "bdew_status": "M",
+        "standard_maximale_wiederholungen": 1,
+        "bdew_maximale_wiederholungen": 1,
+        "ebene": 0,
+        "inhalt": "Nachrichten-Kopfsegment"
+        }
     """
 
     zaehler: str
