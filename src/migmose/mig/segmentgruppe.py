8595a4c1c1196f17c897b6a1911cdf68e7036e3c
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-03-05 19:53:08 +0100

    ➕method for segmentgroup function -> generates structured tables

--- src/migmose/mig/segmentgruppe.py
+++ src/migmose/mig/segmentgruppe.py
@@ -2,11 +2,15 @@
 contains class for structured segmentgroups in mig tables. Builds table recursively.
 """
 
+from typing import Optional, Tuple
+
+from pydantic import BaseModel
+
 from migmose.mig.nachrichtenstruktur import NachrichtenstrukturTabelle
 from migmose.mig.nachrichtenstrukturzeile import NachrichtenstrukturZeile
 
 
-class SegmentGruppe(NachrichtenstrukturZeile):
+class SegmentGruppe(BaseModel):
     """
     class for structured segmentgroups in mig tables. Builds table recursively. Inherits from NachrichtenstrukturZeile
     e.g.(ORDCHG):
@@ -56,19 +60,40 @@ class SegmentGruppe(NachrichtenstrukturZeile):
     }
     """
 
-    segmente: list[NachrichtenstrukturZeile]
-    segmentgruppen: list["SegmentGruppe"]
+    header_linie: Optional[NachrichtenstrukturZeile] = None
+    segmente: list[Optional[NachrichtenstrukturZeile]] = []
+    segmentgruppen: list[Optional["SegmentGruppe"]] = []
 
     @classmethod
-    def structure_table(cls, table: NachrichtenstrukturTabelle, index: int = 0) -> "SegmentGruppe":
-        collected_segments: list[NachrichtenstrukturZeile] = []
-        collected_segmentgroups: list["SegmentGruppe"] = []
-        for i, line in enumerate(table.lines[index:]):
+    def structure_table(
+        cls, table: NachrichtenstrukturTabelle, segmentgruppe: Optional[NachrichtenstrukturZeile] = None, index: int = 0
+    ) -> Tuple["SegmentGruppe", int]:
+        """init structured table"""
+        collected_segments: list[Optional[NachrichtenstrukturZeile]] = []
+        collected_segmentgroups: list[Optional["SegmentGruppe"]] = []
+        i = index
+        while i < len(table.lines):
+            line = table.lines[i]
             is_line_segmentgruppe = line.nr is None
-            if not is_line_segmentgruppe:
-                collected_segments.append(line)
-            elif is_line_segmentgruppe and line.ebene == table.lines[i - 1].ebene + 1:
-                collected_segmentgroups.append(cls.structure_table(table, i + 1))
+            if is_line_segmentgruppe:
+                added_segmentgroup, i = cls.structure_table(table, line, i + 1)
+                collected_segmentgroups.append(added_segmentgroup)
             else:
-                return cls(segmente=collected_segments, segmentgruppen=collected_segmentgroups)
-        return cls(segmente=collected_segments, segmentgruppen=collected_segmentgroups)
+                collected_segments.append(line)
+                i += 1
+            if i < len(table.lines) - 1:
+                next_line_segmentgruppe = table.lines[i].nr is None
+
+                if segmentgruppe is not None and (
+                    (next_line_segmentgruppe and table.lines[i].ebene <= segmentgruppe.ebene)  # type: ignore [operator]
+                    or (line.ebene > table.lines[i].ebene)  # type: ignore [operator]
+                ):
+                    return (
+                        cls(
+                            header_linie=segmentgruppe,
+                            segmente=collected_segments,
+                            segmentgruppen=collected_segmentgroups,
+                        ),
+                        i,
+                    )
+        return cls(header_linie=segmentgruppe, segmente=collected_segments, segmentgruppen=collected_segmentgroups), i
--- unittests/test_segmentgruppe.py
+++ unittests/test_segmentgruppe.py
@@ -14,5 +14,6 @@ class TestSegmentgruppe:
         file_path = Path("unittests/test_data/ORDCHG_MIG_1_1_info_20230331_v2.docx")
         raw_lines = parse_raw_nachrichtenstrukturzeile(file_path)
         nachrichtenstrukturtabelle = NachrichtenstrukturTabelle.init_raw_table(raw_lines)
-        segmentgruppe = SegmentGruppe.structure_table(nachrichtenstrukturtabelle)
-        assert len(segmentgruppe[0].zaehler) == "0010"
+        segmentgruppe, _ = SegmentGruppe.structure_table(nachrichtenstrukturtabelle)
+        assert len(segmentgruppe.segmente) == 5
+        assert len(segmentgruppe.segmentgruppen[3].segmentgruppen) == 1
