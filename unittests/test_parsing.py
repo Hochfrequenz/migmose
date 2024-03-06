406d29e106235c0ff51e9efe91b4d354139896d0
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-03-06 18:51:47 +0100

    added tests for parsing module

--- unittests/test_parsing.py
+++ unittests/test_parsing.py
@@ -2,11 +2,14 @@
 Test parsing routines.
 """
 
+import json
+import logging
 from pathlib import Path
 
 from maus.edifact import EdifactFormat
+from pytest_loguru.plugin import caplog  # type: ignore[import] # pylint: disable=unused-import
 
-from migmose.parsing import find_file_to_type
+from migmose.parsing import find_file_to_type, parse_raw_nachrichtenstrukturzeile, preliminary_output_as_json
 
 
 class TestParsing:
@@ -18,19 +21,34 @@ class TestParsing:
         message_type = [EdifactFormat.ORDCHG]
         input_dir = Path("unittests/test_data/")
         file_dict = find_file_to_type(message_type, input_dir)
-        assert file_dict[message_type[0]] == input_dir / Path("ORDCHG_MIG_1_1_info_20230331_v2.docx")
+        assert file_dict[EdifactFormat.ORDCHG] == input_dir / Path("ORDCHG_MIG_1_1_info_20230331_v2.docx")
 
-        # for message_type in message_types:
-        #    list_of_all_files = [
-        #        file for file in input_dir.iterdir() if message_type in file.name and file.suffix == ".docx"
-        #    ]
-        #    if len(list_of_all_files) == 1:
-        #        file_dict[message_type] = list_of_all_files[0]
-        #    elif len(list_of_all_files) > 1:
-        #        logger.warning(f"⚠️ There are several files for {message_type}.", fg="red")
-        #    else:
-        #        logger.warning(f"⚠️ No file found for {message_type}.", fg="red")
-        # if file_dict:
-        #    return file_dict
-        # logger.error("⚠️ No files found in the input directory.", fg="red")
-        # raise click.Abort()
+    def test_find_only_one_file(self, caplog):
+        message_type = [EdifactFormat.ORDCHG, EdifactFormat.ORDRSP]
+        input_dir = Path("unittests/test_data/")
+        with caplog.at_level(logging.WARNING):
+            file_dict = find_file_to_type(message_type, input_dir)
+            assert f"No file found for {EdifactFormat.ORDRSP}." in caplog.text
+            assert file_dict[EdifactFormat.ORDCHG] == input_dir / Path("ORDCHG_MIG_1_1_info_20230331_v2.docx")
+
+    def test_parse_raw_nachrichtenstrukturzeile(self):
+
+        input_file = Path("unittests/test_data/ORDCHG_MIG_1_1_info_20230331_v2.docx")
+        mig_table = parse_raw_nachrichtenstrukturzeile(input_file)
+        assert len(mig_table) == 18
+        assert "Nachrichten-Kopfsegment" in mig_table[0]
+        assert "Nachrichten-Endesegment" in mig_table[-1]
+
+    def test_preliminary_output_as_json(self, tmp_path):
+        table = ["line1", "line2", "line3"]
+        message_type = EdifactFormat.ORDCHG
+        output_dir = tmp_path / Path("output")
+
+        preliminary_output_as_json(table, message_type, output_dir)
+
+        file_path = output_dir / f"{message_type}_preliminary_output.json"
+        assert file_path.exists()
+
+        with open(file_path, "r", encoding="utf-8") as json_file:
+            content = json.load(json_file)
+            assert content == {"line1": None, "line2": None, "line3": None}
