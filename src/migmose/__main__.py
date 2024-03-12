109bcb4bec527ff08fa10eee0c7266a28907f9ac
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-03-12 13:14:58 +0100

    renamed NachrichtenTYPE -> NachrichtenFORMAT

--- src/migmose/__main__.py
+++ src/migmose/__main__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import click
 from maus.edifact import EdifactFormat
 
-from migmose.parsing import find_file_to_type, parse_raw_nachrichtenstrukturzeile, preliminary_output_as_json
+from migmose.parsing import find_file_to_format, parse_raw_nachrichtenstrukturzeile, preliminary_output_as_json
 
 
 # add CLI logic
@@ -21,11 +21,11 @@ from migmose.parsing import find_file_to_type, parse_raw_nachrichtenstrukturzeil
 )
 @click.option(
     "-mt",
-    "--message_type",
+    "--message_format",
     type=click.Choice(list(map(lambda x: x.name, EdifactFormat)), case_sensitive=False),
     # Taken from https://github.com/pallets/click/issues/605#issuecomment-889462570
-    prompt="Please specify which message type to be parsed.",
-    help="Defines the set of message types to be parsed.",
+    prompt="Please specify which message format to be parsed.",
+    help="Defines the set of message formats to be parsed.",
     multiple=True,
 )
 @click.option(
@@ -35,16 +35,16 @@ from migmose.parsing import find_file_to_type, parse_raw_nachrichtenstrukturzeil
     prompt="Please enter the path to the directory which should contain the output files.",
     help="Set path to directory which contains the output files. If the directory does not exist, it will be created.",
 )
-def main(input_dir: Path, output_dir, message_type: list[EdifactFormat]) -> None:
+def main(input_dir: Path, output_dir, message_format: list[EdifactFormat]) -> None:
     """
     Main function. Uses CLI input.
     """
-    dict_files = find_file_to_type(message_type, input_dir)
-    for m_type, file in dict_files.items():
+    dict_files = find_file_to_format(message_format, input_dir)
+    for m_format, file in dict_files.items():
         mig_table = parse_raw_nachrichtenstrukturzeile(file)
         for item in mig_table:
             print(item)
-        preliminary_output_as_json(mig_table, m_type, output_dir)
+        preliminary_output_as_json(mig_table, m_format, output_dir)
 
 
 if __name__ == "__main__":
--- unittests/test_parsing.py
+++ unittests/test_parsing.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from maus.edifact import EdifactFormat
 from pytest_loguru.plugin import caplog  # type: ignore[import] # pylint: disable=unused-import
 
-from migmose.parsing import find_file_to_type, parse_raw_nachrichtenstrukturzeile, preliminary_output_as_json
+from migmose.parsing import find_file_to_format, parse_raw_nachrichtenstrukturzeile, preliminary_output_as_json
 
 
 class TestParsing:
@@ -17,17 +17,17 @@ class TestParsing:
     Test class for parsing functions.
     """
 
-    def test_find_file_to_type(self):
+    def test_find_file_to_format(self):
         message_type = [EdifactFormat.ORDCHG]
         input_dir = Path("unittests/test_data/")
-        file_dict = find_file_to_type(message_type, input_dir)
+        file_dict = find_file_to_format(message_type, input_dir)
         assert file_dict[EdifactFormat.ORDCHG] == input_dir / Path("ORDCHG_MIG_1_1_info_20230331_v2.docx")
 
     def test_find_only_one_file(self, caplog):
-        message_type = [EdifactFormat.ORDCHG, EdifactFormat.ORDRSP]
+        message_formats = [EdifactFormat.ORDCHG, EdifactFormat.ORDRSP]
         input_dir = Path("unittests/test_data/")
         with caplog.at_level(logging.WARNING):
-            file_dict = find_file_to_type(message_type, input_dir)
+            file_dict = find_file_to_format(message_formats, input_dir)
             assert f"No file found for {EdifactFormat.ORDRSP}." in caplog.text
             assert file_dict[EdifactFormat.ORDCHG] == input_dir / Path("ORDCHG_MIG_1_1_info_20230331_v2.docx")
 
@@ -41,12 +41,12 @@ class TestParsing:
 
     def test_preliminary_output_as_json(self, tmp_path):
         table = ["line1", "line2", "line3"]
-        message_type = EdifactFormat.ORDCHG
+        message_format = EdifactFormat.ORDCHG
         output_dir = tmp_path / Path("output")
 
-        preliminary_output_as_json(table, message_type, output_dir)
+        preliminary_output_as_json(table, message_format, output_dir)
 
-        file_path = output_dir / f"{message_type}_preliminary_output.json"
+        file_path = output_dir / f"{message_format}_preliminary_output.json"
         assert file_path.exists()
 
         with open(file_path, "r", encoding="utf-8") as json_file:
