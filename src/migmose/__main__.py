879c047085b24203521afed81e9f9a86d525e95d
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-03-13 12:15:31 +0100

    ➕💾 added CLI output format options

--- src/migmose/__main__.py
+++ src/migmose/__main__.py
@@ -5,6 +5,7 @@ contains CLI logic for migmose.
 from pathlib import Path
 
 import click
+from loguru import logger
 from maus.edifact import EdifactFormat
 
 from migmose.mig.nachrichtenstrukturtabelle import NachrichtenstrukturTabelle
@@ -22,7 +23,7 @@ from migmose.parsing import find_file_to_format, parse_raw_nachrichtenstrukturze
     help="Set path to directory which contains the .docx files for the migs",
 )
 @click.option(
-    "-mt",
+    "-mf",
     "--message_format",
     type=click.Choice(list(map(lambda x: x.name, EdifactFormat)), case_sensitive=False),
     # Taken from https://github.com/pallets/click/issues/605#issuecomment-889462570
@@ -37,19 +38,43 @@ from migmose.parsing import find_file_to_format, parse_raw_nachrichtenstrukturze
     prompt="Please enter the path to the directory which should contain the output files.",
     help="Set path to directory which contains the output files. If the directory does not exist, it will be created.",
 )
-def main(input_dir: Path, output_dir, message_format: list[EdifactFormat]) -> None:
+@click.option(
+    "-ft",
+    "--file_type",
+    type=click.Choice(["csv", "nested_json"], case_sensitive=False),
+    default=["csv"],
+    prompt="Please specify how the output should be formatted.",
+    help="Defines the output format. Choose between csv and nested_json. Default is csv.",
+    multiple=True,
+)
+def main(input_dir: Path, output_dir, message_format: list[EdifactFormat], file_type: list[str]) -> None:
     """
     Main function. Uses CLI input.
     """
+    if message_format is None:
+        message = f"❌ No message format specified. Please specify the message format."
+        click.secho(message, fg="yellow")
+        logger.error(message)
+        raise click.Abort()
+    if file_type is None:
+        message = f"❌ No output format specified. Please specify the output format."
+        click.secho(message, fg="yellow")
+        logger.error(message)
+        raise click.Abort()
+
     dict_files = find_file_to_format(message_format, input_dir)
     for m_format, file in dict_files.items():
         raw_lines = parse_raw_nachrichtenstrukturzeile(file)
         nachrichtenstrukturtabelle = NachrichtenstrukturTabelle.create_nachrichtenstruktur_tabelle(raw_lines)
-        nachrichtenstrukturtabelle.to_csv(m_format, output_dir)
+        if "csv" in file_type:
+            logger.info("💾 Saving flat Nachrichtenstruktur table as csv.")
+            nachrichtenstrukturtabelle.to_csv(m_format, output_dir)
         nested_nachrichtenstruktur, _ = NestedNachrichtenstruktur.create_nested_nachrichtenstruktur(
             nachrichtenstrukturtabelle
         )
-        nested_nachrichtenstruktur.to_json(m_format, output_dir)
+        if "nested_json" in file_type:
+            logger.info("💾 Saving nested Nachrichtenstruktur as json.")
+            nested_nachrichtenstruktur.to_json(m_format, output_dir)
 
 
 if __name__ == "__main__":
