7c37b07ccaf88b8e0b2e1e882a9368ce8199a80c
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2024-03-13 15:17:15 +0100

    added comment

--- src/migmose/__main__.py
+++ src/migmose/__main__.py
@@ -28,7 +28,7 @@ from migmose.parsing import find_file_to_format, parse_raw_nachrichtenstrukturze
     type=click.Choice(list(map(lambda x: x.name, EdifactFormat)), case_sensitive=False),
     # Taken from https://github.com/pallets/click/issues/605#issuecomment-889462570
     default=list(map(lambda x: x.name, EdifactFormat)),
-    help="Defines the set of message formats to be parsed.",
+    help="Defines the set of message formats to be parsed. If no format is specified, all formats are parsed.",
     multiple=True,
 )
 @click.option(
