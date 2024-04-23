e19817d686693bfa4bf85e0ed5f4afa4e1a2874f
Hamid <hamid.github@hochfrequenz.de>
2024-04-23 16:46:28 +0200

    adding unittest

---
+++ unittests/test_create_reduced_nested_nachrichtenstruktur.py
@@ -0,0 +1,53 @@
+import unittest
+from pathlib import Path
+from typing import Any
+
+from maus.edifact import EdifactFormat
+from pydantic import BaseModel
+
+from migmose.mig.reducednestednachrichtenstruktur import ReducedNestedNachrichtenstruktur
+
+
+class TestReducedNestedNachrichtenstruktur(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.input_data = {
+            "header_linie": None,
+            "segmente": [
+                {"zaehler": "101", "bezeichnung": "Segment1", "bdew_status": "D"},
+                {"zaehler": "101", "bezeichnung": "Segment1", "bdew_status": "R"},
+                {"zaehler": "102", "bezeichnung": "Segment2"},
+            ],
+            "segmentgruppen": [
+                {
+                    "header_linie": {"zaehler": "201", "bezeichnung": "SegmentGroup1"},
+                    "segmente": [{"zaehler": "103", "bezeichnung": "Segment3"}],
+                }
+            ],
+        }
+
+    def test_create_reduced_nested_nachrichtenstruktur(self):
+        """test if the reduced nested nachrichtenstruktur is created correctly"""
+        expected_output = {
+            "header_linie": None,
+            "segmente": [
+                {"zaehler": "101", "bezeichnung": "Segment1", "bdew_status": "D"},
+                {"zaehler": "102", "bezeichnung": "Segment2"},
+            ],
+            "segmentgruppen": [
+                {
+                    "header_linie": {"zaehler": "201", "bezeichnung": "SegmentGroup1"},
+                    "segmente": [{"zaehler": "103", "bezeichnung": "Segment3"}],
+                    "segmentgruppen": [],
+                }
+            ],
+        }
+
+        output = ReducedNestedNachrichtenstruktur.create_reduced_nested_nachrichtenstruktur(
+            json_nachrichten_struktur=self.input_data
+        )
+
+        self.assertEqual(output, expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
